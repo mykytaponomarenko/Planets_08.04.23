84fd5ede0ce1297f0d96be2e05020bfb1e0128eb
MykytaPonomarenko <mykyta.ponomarenko@gmail.com>
2023-04-08 19:30:53 +0300

    added svg. it changes for each ball

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const g = 0.1;
 const fps = 60;
 const balls = [];
 const forceY = g;
-const forceX = g*0.2;
+const forceX = g;
 
 document.addEventListener('click', handleMouseClick) 
 
@@ -10,17 +10,31 @@ let ballNumber = 0;
 
 
 function handleMouseClick(event) {
+
     ballNumber += 1;
-    const id = 'ball-' + ballNumber 
-    document.body.innerHTML += `<img src = "Great_ball.webp" id="${id}">`
+    const id = 'ball-' + ballNumber; 
+
+    let imgElement = document.createElement('img');
+    imgElement.id = id ;
+    imgElement.src = 'Great_ball.webp';
+
+    let lineElement = document.createElement('line');
+    lineElement.setAttribute('x1', "0");
+    lineElement.setAttribute('y1', "80");
+    lineElement.setAttribute('x2', "100");
+    lineElement.setAttribute('y2', "20");
+    lineElement.setAttribute('stroke', "0");
+
+    document.body.append(imgElement);
+    document.querySelector('#svg').innerHTML = `<line x1="${ballNumber*10}" y1="80" x2="100" y2="20" stroke="black" />`
 
     const newBall = {
         x: event.x,
         y: event.y,
         width: 100,
         height: 100,
-        vx: 50,
-        vy: 5,
+        vx: 10,
+        vy: 10,
         element: document.querySelector('#' + id),
       }
 
@@ -28,15 +42,24 @@ function handleMouseClick(event) {
 
 } 
 
-function move(ball) {
+function move(ball, idx) {
     ball.vx += forceX;
     ball.vy += forceY; 
 
-    if (ball.y > window.innerHeight - 50) { 
+    if (ball.y > window.innerHeight - 5) { 
         ball.vy = -ball.vy * 0.97;
       
     }   
-    if (ball.x > window.innerWidth-50) { 
+    if (ball.x > window.innerWidth-5) { 
+        ball.vx = -ball.vx * 0.9;
+      
+    } 
+
+    if (ball.y < 5) { 
+        ball.vy = -ball.vy * 0.97;
+      
+    }   
+    if (ball.x < 5) { 
         ball.vx = -ball.vx * 0.9;
       
     } 
@@ -44,8 +67,12 @@ function move(ball) {
     ball.x += ball.vx;
     ball.y += ball.vy;
 
-    console.log("speed y: " + ball.vy);
+
+    console.log("ball #" + idx, "\n" + "speed y: " + ball.vy);
     console.log("speed x: " + ball.vx);
+
+    console.log("y: " + ball.y);
+    console.log("x: " + ball.x);
 
 }
 
@@ -55,7 +82,7 @@ function drawBall(ball) {
     ball.element.style.top = ball.y + "px";
   }
 
-function time() {    
+function time() {   
     balls.forEach(move);
     balls.forEach(drawBall);
 
