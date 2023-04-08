11ff3021be5647a579e2c964569723734c4b5f90
MykytaPonomarenko <mykyta.ponomarenko@gmail.com>
2023-04-08 21:13:13 +0300

    add first ball. On click a line is created. x1,y1 from event. x2,y2 to last created ball

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const balls = [];
 const forceY = g;
 const forceX = g;
 
+let imgElement = document.createElement('img');
+    imgElement.id = "zero" ;
+    imgElement.src = 'Great_ball.webp';
+
+    document.body.append(imgElement);
+
+const firstBall = {
+    x: 100,
+    y: 100,
+    width: 50,
+    height: 50,
+    vx: 0,
+    vy: 0,
+    element: document.querySelector('#zero'),
+  }
+
+  balls.push(firstBall);
+
 document.addEventListener('click', handleMouseClick) 
 
 let ballNumber = 0;
@@ -14,31 +32,34 @@ function handleMouseClick(event) {
     ballNumber += 1;
     const id = 'ball-' + ballNumber; 
 
+    let lastAddedBall = balls[balls.length-1];
+
     let imgElement = document.createElement('img');
     imgElement.id = id ;
     imgElement.src = 'Great_ball.webp';
 
     let lineElement = document.createElementNS("http://www.w3.org/2000/svg", 'line');
-    lineElement.setAttribute('x1', event.x);
-    lineElement.setAttribute('y1', event.y);
-    lineElement.setAttribute('x2', event.x*2);
-    lineElement.setAttribute('y2', event.y*2);
+    lineElement.setAttribute('x1', `${event.x}`);
+    lineElement.setAttribute('y1', `${event.y}`);
+    lineElement.setAttribute('x2', `${lastAddedBall.x}`);
+    lineElement.setAttribute('y2', `${lastAddedBall.y}`);
     lineElement.setAttribute('stroke', "black");
     console.log("event.x: ", event.x);
     console.log("event.y: ", event.y);
 
     document.body.append(imgElement);
-    let svgElemet = document.querySelector('#svg');
-    svgElemet.append((lineElement));
-    svgElemet.setAttribute('viewBox', `0 0 ${window.innerWidth} ${window.innerHeight}`);    
+    let svgElement = document.querySelector('#svg');
+    svgElement.append((lineElement));
+    svgElement.setAttribute('viewBox', `0 0 ${window.innerWidth} ${window.innerHeight}`); 
+         
 
     const newBall = {
         x: event.x,
         y: event.y,
-        width: 100,
-        height: 100,
-        vx: 10,
-        vy: 10,
+        width: 50,
+        height: 50,
+        vx: 0,
+        vy: 0,
         element: document.querySelector('#' + id),
       }
 
